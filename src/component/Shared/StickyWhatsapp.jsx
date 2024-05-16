68655906dc916a1fdaf27bdf7f9cc2869d6b5261
niks-nikhil-anand <niks.anand.developer@gmail.com>
2024-05-16 15:08:15 +0530

    Fix some bugs in the whatsapp component

--- src/component/Shared/StickyWhatsapp.jsx
+++ src/component/Shared/StickyWhatsapp.jsx
@@ -6,7 +6,7 @@ import Link from 'next/link';
 export default function WhatsappIcon() {
   return (
     <motion.div
-      className="fixed bottom-4 right-4 "
+      className="fixed bottom-4 right-4 z-20"
       initial={{ opacity: 0, scale: 0.5 }}
       animate={{ opacity: 1, scale: 1 }}
       transition={{ duration: 0.5 }}
