4584fd773a36111b7d75aae59420e8bb1f575ab5
niks-nikhil-anand <niks.anand.developer@gmail.com>
2024-05-28 18:17:00 +0530

    Modify padding

--- src/app/(gallery)/videogallery/page.jsx
+++ src/app/(gallery)/videogallery/page.jsx
@@ -12,7 +12,7 @@ const YouTubePlayer = () => {
     </div>
 
    
-    <div className="md:p-[3rem]">
+    <div className="">
     <div className="videowrapper relative w-full pb-[56.25%] pt-6 h-0">
      <iframe  src="https://www.youtube-nocookie.com/embed/wdgH3_SXUZk?si=ldd-npOEMO82o1Mi" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
     </div>
