2c52be692c83c30bad6252d2b056c68f831de098
niks-nikhil-anand <niks.anand.developer@gmail.com>
2024-06-01 13:30:18 +0530

    Added CSP , DSA , Branch APi

---
+++ src/app/api/user/csp/route.js
@@ -0,0 +1,21 @@
+import connectDB from "@/lib/dbConnect";
+import partnerApplication from "@/models/partnerApplication";
+import { NextResponse } from "next/server";
+
+export const GET = async (req, res) => {
+    try {
+      await connectDB();
+  
+      const applications = await partnerApplication.find({ role: "CSP" });
+      console.log(applications)
+  
+      return NextResponse.json(applications, {
+        status: 200
+      });
+    } catch (error) {
+      console.error('Error fetching applications:', error);
+      return NextResponse.json({ msg: "Error fetching applications", error: error.message }, {
+        status: 500
+      });
+    }
+  };
