85a91cd7db8ef7d849fc5d13ae40fcda249162b3
niks-nikhil-anand <niks.anand.developer@gmail.com>
2024-06-01 16:09:24 +0530

    Fix some typo

--- src/models/partnerApplication.js
+++ src/models/partnerApplication.js
@@ -1,69 +1,71 @@
-import mongoose from "mongoose";
+  import mongoose from "mongoose";
 
-// Define Schema
-const partnerApplicationSchema = new mongoose.Schema({
-  name: { 
-    type: String, 
-    required: true 
-},
-  email: { 
+  // Define Schema
+  const partnerApplicationSchema = new mongoose.Schema({
+    name: { 
+      type: String, 
+      required: true 
+  },
+    email: { 
+      type: String, 
+      required: true,
+      unique : true,
+      match: [/.+\@.+\..+/, 'Please fill a valid email address']
+  },
+  username:{
     type: String, 
     required: true,
-    match: [/.+\@.+\..+/, 'Please fill a valid email address']
- },
- username:{
-  type: String, 
-  required: true,
- },
-  mobileNumber: { 
-    type: String, 
-    required: true 
-},
-  password: { 
-    type: String, 
-    required: true 
-},
-  city: { 
-    type: String
- },
-  state: { 
-    type: String
- },
-  partnerType: { 
-    type: String,
-    enum: ['CSP', 'Branch', 'DSA'],
-},
-  interest: { 
-    type: String 
-},
-  message: { 
-    type: String 
-},
-  aadhaarCard: { 
-    type: String 
-},
-  panCard: { 
-    type: String 
-},
-  bankPassbook: { 
-    type: String 
-},
-  shopPhotoCopy: { 
-    type: String 
-},
-  msmeCertificate: { 
-    type: String 
-},
-role:{
-    type : String,
-    enum: ['CSP', 'Branch', 'DSA' , 'User' , 'Admin'],
-    default: 'User'
-},
-  isApproved: { 
-    type: Boolean, 
-    default: false 
-}, 
-});
+    unique : true
+  },
+    mobileNumber: { 
+      type: String, 
+      required: true 
+  },
+    password: { 
+      type: String, 
+      required: true 
+  },
+    city: { 
+      type: String
+  },
+    state: { 
+      type: String
+  },
+    partnerType: { 
+      type: String,
+      enum: ['CSP', 'Branch', 'DSA'],
+  },
+    interest: { 
+      type: String 
+  },
+    message: { 
+      type: String 
+  },
+    aadhaarCard: { 
+      type: String 
+  },
+    panCard: { 
+      type: String 
+  },
+    bankPassbook: { 
+      type: String 
+  },
+    shopPhotoCopy: { 
+      type: String 
+  },
+    msmeCertificate: { 
+      type: String 
+  },
+  role:{
+      type : String,
+      enum: ['CSP', 'Branch', 'DSA' , 'User' , 'Admin'],
+      default: 'User'
+  },
+    isApproved: { 
+      type: Boolean, 
+      default: false 
+  }, 
+  });
 
-export default mongoose.models.PartnerApplication || mongoose.model('PartnerApplication' , partnerApplicationSchema )
+  export default mongoose.models.PartnerApplication || mongoose.model('PartnerApplication' , partnerApplicationSchema )
 
