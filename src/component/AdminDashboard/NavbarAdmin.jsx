eaf54050275a40f179ad50cf08fe5f29510717f8
niks-nikhil-anand <niks.anand.developer@gmail.com>
2024-06-01 11:32:52 +0530

    added navbar

--- src/component/AdminDashboard/NavbarAdmin.jsx
+++ src/component/AdminDashboard/NavbarAdmin.jsx
@@ -21,23 +21,23 @@ const NavbarAdmin = ({ toggleSidebar }) => {
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
         </svg>
       </button>
-      <div className="hidden md:flex items-center space-x-4">
+      <div className="hidden md:flex items-center gap-[2rem]">
         <button onClick={toggleDarkMode} className="p-2">
           {isDarkMode ? (
-           <MdLightMode />
+           <MdLightMode className="w-8 h-8" />
           ) : (
-            <MdDarkMode />
+            <MdDarkMode className="w-8 h-8" />
           )}
         </button>
-        <button className="p-2">
-        <IoIosNotifications />
+        <button className="">
+        <IoIosNotifications className="w-8 h-8" />
         </button>
-        <button className="p-2">
-        <IoSettings />
+        <button className="">
+        <IoSettings className="w-8 h-8" />
         </button>
         <div className="relative group">
-          <button className="p-2">
-          <CgProfile />
+          <button className="">
+          <CgProfile className="w-8 h-8" />
           </button>
           <div className="hidden group-hover:block absolute right-0 mt-2 w-48 bg-white dark:bg-gray-800 border rounded shadow-lg z-20">
             <a href="#" className="block px-4 py-2 text-gray-800 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700">Profile</a>
