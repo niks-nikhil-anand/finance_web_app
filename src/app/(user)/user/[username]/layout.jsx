2ba428aac55d74fddfb30cdb20adb24ff030e9e6
niks-nikhil-anand <niks.anand.developer@gmail.com>
2024-06-02 06:27:55 +0530

    Added Admin controller

---
+++ src/app/(user)/user/[username]/layout.jsx
@@ -0,0 +1,13 @@
+import NavbarUser from "@/component/User/NavbarUser";
+
+const Layout = ({ children }) => {
+  return (
+        <div>
+         
+          <NavbarUser />
+          
+        </div>
+  );
+}
+
+export default Layout;
---
+++ src/component/User/NavbarUser.jsx
@@ -0,0 +1,39 @@
+"use client"
+import React, { useState, useEffect } from 'react';
+import { motion } from 'framer-motion';
+import axios from 'axios';
+
+const NavbarUser = () => {
+  const [username, setUsername] = useState('');
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('/api/user/username');
+        setUsername(response.data.username);
+      } catch (error) {
+        console.error('Error fetching username:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <nav className="bg-gray-800 p-4">
+      <div className="max-w-7xl mx-auto flex justify-between items-center">
+        <div className="text-white font-bold text-lg">Legal 257 - DSA</div>
+        <motion.div
+          className="text-white"
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.9 }}
+        >
+         {/* {username && <span>Welcome, {username}</span>} */}
+         <span>Welcome , Nikhil</span>
+        </motion.div>
+      </div>
+    </nav>
+  );
+};
+
+export default NavbarUser;
