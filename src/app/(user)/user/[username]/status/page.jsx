ff702df961064005f28f255489bf847aee2db82c
niks-nikhil-anand <niks.anand.developer@gmail.com>
2024-06-11 07:07:40 +0530

    Added some mongoose model

---
+++ src/app/(user)/user/[username]/status/page.jsx
@@ -0,0 +1,56 @@
+"use client"
+import React, { useEffect, useState } from 'react';
+import { motion } from 'framer-motion';
+import axios from 'axios';
+
+const ApplicationStatus = () => {
+  const [application, setApplication] = useState({ name: '', status: '' });
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const statusColors = {
+    'Details Pending': 'bg-yellow-500',
+    'Under Processing': 'bg-blue-500',
+    'Approved': 'bg-green-500',
+    'Rejected': 'bg-red-500',
+  };
+
+  useEffect(() => {
+    const fetchStatus = async () => {
+      try {
+        const response = await axios.get('/api/user/status');
+        setApplication(response.data);
+        setLoading(false);
+      } catch (err) {
+        setError(err);
+        setLoading(false);
+      }
+    };
+
+    fetchStatus();
+  }, []);
+
+  if (loading) return <div className="text-center">Loading...</div>;
+  if (error) return <div className="text-center text-red-500">Error loading status</div>;
+
+  return (
+    <motion.div
+      className="max-w-sm mx-auto my-4 p-4 rounded-lg shadow-lg flex flex-col items-center"
+      initial={{ opacity: 0, scale: 0.9 }}
+      animate={{ opacity: 1, scale: 1 }}
+      transition={{ duration: 0.3 }}
+    >
+      <h1 className="text-2xl font-bold mb-2">{application.name}</h1>
+      <motion.div
+        className={`w-full text-center text-white py-2 rounded ${statusColors[application.status]}`}
+        initial={{ opacity: 0, y: 50 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5 }}
+      >
+        {application.status}
+      </motion.div>
+    </motion.div>
+  );
+};
+
+export default ApplicationStatus;
---
+++ src/models/complaintUser.js
@@ -0,0 +1,48 @@
+import mongoose from "mongoose";
+
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true,
+    lowercase: true,
+    validate: {
+      validator: function(v) {
+        return /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v);
+      },
+      message: props => `${props.value} is not a valid email!`
+    }
+  },
+  phoneNumber: {
+    type: String,
+    required: true,
+    trim: true,
+    validate: {
+      validator: function(v) {
+        return /^\d{10}$/.test(v);
+      },
+      message: props => `${props.value} is not a valid phone number!`
+    }
+  },
+  message: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+},{
+    partner:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'PartnerApplication'
+    }
+},{
+  timestamps: true,
+});
+
+module.exports = mongoose.model('User', userSchema);
