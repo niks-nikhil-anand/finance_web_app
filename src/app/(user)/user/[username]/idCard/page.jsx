6375eaa7f8f7a8adae135c618b3350a961986dca
niks-nikhil-anand <niks.anand.developer@gmail.com>
2024-06-29 12:28:25 +0530

    Added back button

--- src/app/(user)/user/[username]/idCard/page.jsx
+++ src/app/(user)/user/[username]/idCard/page.jsx
@@ -2,12 +2,12 @@
 import React, { useEffect, useState, useRef } from 'react';
 import axios from 'axios';
 import { motion } from 'framer-motion';
-import logo from '../../../../../../public/logo2.png';
-import legalqr from '../../../../../../public/logo/legalqr.png';
-import barcode from '../../../../../../public/barcode.gif';
 import Image from 'next/image';
 import { FaArrowLeft } from 'react-icons/fa';
 import { toPng } from 'html-to-image';
+import logo from '../../../../../../public/logo2.png';
+import legalqr from '../../../../../../public/logo/legalqr.png';
+import barcode from '../../../../../../public/barcode.gif';
 
 const IdCard = () => {
   const [user, setUser] = useState(null);
@@ -43,7 +43,7 @@ const IdCard = () => {
 
   const downloadIdCard = () => {
     if (cardRef.current) {
-      toPng(cardRef.current)
+      toPng(cardRef.current, { cacheBust: true, useCORS: true })
         .then((dataUrl) => {
           const link = document.createElement('a');
           link.href = dataUrl;
@@ -57,106 +57,104 @@ const IdCard = () => {
   };
 
   return user ? (
-    <div>
-       <div>
-                <button onClick={() => window.history.back()} className="text-xl text-red-700 border-black rounded-full p-3">
-                    <FaArrowLeft />
-                </button>
-            </div>
+    <div className='bg-gray-100 p-5'>
+      <div>
+        <button onClick={() => window.history.back()} className="text-xl text-red-700 border-black rounded-full p-3">
+          <FaArrowLeft />
+        </button>
+      </div>
       <div className="flex justify-center items-center min-h-screen bg-gray-100 p-5">
-     
-     <div className="flex flex-col md:flex-row items-center space-y-4 md:space-y-0 md:space-x-4 ">
-       {/* Front Side of the Card */}
-       <motion.div
-         className="w-full md:w-64 h-[30rem] bg-white rounded-lg shadow-2xl overflow-hidden relative"
-         initial={{ opacity: 0 }}
-         animate={{ opacity: 1 }}
-         transition={{ duration: 0.5 }}
-       >
-         <div className="bg-purple-600 h-[8rem] flex justify-center items-center relative">
-           <Image
-             src={"https://via.placeholder.com/100"}
-             alt="Profile"
-             height={100}
-             width={100}
-             className="w-24 h-24 rounded-full border-4 border-white absolute bottom-[-50%] transform translate-y-[-50%]"
-           />
-         </div>
-         <div className="p-4 text-center mt-8">
-           <h2 className="text-xl  text-gradient-blue font-extrabold">{user.name}</h2>
-           <p className="text-gray-600">{user.role}</p>
-           <div className="mt-4">
-             <Image
-              src={legalqr}
-              height={100}
-              width={100}
-               alt="QR Code"
-               className="mx-auto"
-             />
-           </div>
-           <p className="text-sm">{user.mobile}</p>
-           <p className="text-sm">{user.username}</p>
-           <p className="mt-4 text-sm ">{user.city}, {user.state}</p>
-          
-         </div>
-         <div className=" absolute left-0">
-             <Image
-              src={logo}
-              height={100}
-              width={100}
-               alt="QR Code"
-               className="mx-auto"
-             />
-           </div>
-       </motion.div>
-       
-       {/* Back Side of the Card */}
-       <motion.div
-         className="w-full md:w-64 h-[30rem] bg-purple-600 rounded-lg shadow-lg overflow-hidden relative"
-         initial={{ opacity: 0 }}
-         animate={{ opacity: 1 }}
-         transition={{ duration: 0.5 }}
-       >
-         <div className="p-4">
-           <h2 className="text-xl font-bold text-white text-center underline">Information</h2>
-           <div className="mt-4 text-white">
-             <p className="font-bold">Email:</p>
-             <p>{user.email}</p>
-             <p className="font-bold mt-2">Mobile No:</p>
-             <p>{user.mobile}</p>
-             <p className="font-bold mt-2">Legal257 Id:-</p>
-             <p>{user.id}</p>
-             <p className="font-bold mt-2">Legal257 Services:-</p>
-             <p>{user.services}</p>
-             <p className="font-bold mt-2">Office Address:</p>
-             <p>{user.shop}</p>
-            
-           </div>
-           <div className=" flex justify-center mt-[4rem]  ">
-               <Image
-                 src={barcode}
-                 alt="Barcode"
-                 height={800}
-                 width={800}
-                 className=""
-               />
-             </div>
-         </div>
-         <div className=" absolute left-0 bottom-2">
-             <Image
-              src={logo}
-              height={100}
-              width={100}
-               alt="QR Code"
-               className="mx-auto"
-             />
-           </div>
-       </motion.div>
-      
-     </div>
-   </div>
+        <div ref={cardRef} className="flex flex-col md:flex-row items-center space-y-4 md:space-y-0 md:space-x-4">
+          {/* Front Side of the Card */}
+          <motion.div
+            className="w-full md:w-64 h-[30rem] bg-white rounded-lg shadow-2xl overflow-hidden relative"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.5 }}
+          >
+            <div className="bg-purple-600 h-[8rem] flex justify-center items-center relative">
+              <Image
+                src={user.profilePic || "https://via.placeholder.com/100"}
+                alt="Profile"
+                height={100}
+                width={100}
+                className="w-24 h-24 rounded-full border-4 border-white absolute bottom-[-50%] transform translate-y-[-50%]"
+              />
+            </div>
+            <div className="p-4 text-center mt-8">
+              <h2 className="text-xl text-gradient-blue font-extrabold">{user.name}</h2>
+              <p className="text-gray-600">{user.role}</p>
+              <div className="mt-4">
+                <Image
+                  src={legalqr}
+                  height={100}
+                  width={100}
+                  alt="QR Code"
+                  className="mx-auto"
+                />
+              </div>
+              <p className="text-sm">{user.mobile}</p>
+              <p className="text-sm">{user.username}</p>
+              <p className="mt-4 text-sm">{user.city}, {user.state}</p>
+            </div>
+            <div className="absolute left-0">
+              <Image
+                src={logo}
+                height={100}
+                width={100}
+                alt="QR Code"
+                className="mx-auto"
+              />
+            </div>
+          </motion.div>
+
+          {/* Back Side of the Card */}
+          <motion.div
+            className="w-full md:w-64 h-[30rem] bg-purple-600 rounded-lg shadow-lg overflow-hidden relative"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.5 }}
+          >
+            <div className="p-4">
+              <h2 className="text-xl font-bold text-white text-center underline">Information</h2>
+              <div className="mt-4 text-white">
+                <p className="font-bold">Email:</p>
+                <p>{user.email}</p>
+                <p className="font-bold mt-2">Mobile No:</p>
+                <p>{user.mobile}</p>
+                <p className="font-bold mt-2">Legal257 Id:</p>
+                <p>{user.id}</p>
+                <p className="font-bold mt-2">Legal257 Services:</p>
+                <p>{user.services}</p>
+                <p className="font-bold mt-2">Office Address:</p>
+                <p>{user.shop}</p>
+              </div>
+              <div className="flex justify-center mt-[4rem]">
+                <Image
+                  src={barcode}
+                  alt="Barcode"
+                  height={800}
+                  width={800}
+                  className=""
+                />
+              </div>
+            </div>
+            <div className="absolute left-0 bottom-2">
+              <Image
+                src={logo}
+                height={100}
+                width={100}
+                alt="QR Code"
+                className="mx-auto"
+              />
+            </div>
+          </motion.div>
+        </div>
+      </div>
+      <button onClick={downloadIdCard} className="text-white bg-blue-500 p-2 rounded mx-auto block mt-5">
+        Download IdCard
+      </button>
     </div>
-    
   ) : null;
 };
 
--- src/app/(user)/user/[username]/profile/page.jsx
+++ src/app/(user)/user/[username]/profile/page.jsx
@@ -1,13 +1,14 @@
-"use client";
+"use client"
 import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 import axios from 'axios';
 import { FaArrowLeft } from 'react-icons/fa';
 import Link from 'next/link';
+import blank_profile from '../../../../../../public/blank_profile_pic.png';
 
 const Profile = () => {
   const [user, setUser] = useState(null);
-  
+  const [profilePic, setProfilePic] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -26,7 +27,9 @@ const Profile = () => {
           shopAddress: userData.shopAddress,
           username: userData.username,
           id: userData._id,
+          profilePic: userData.profilePic,
         });
+        setProfilePic(userData.profilePic);
       } catch (error) {
         console.error('Error fetching user data:', error);
       }
@@ -44,6 +47,32 @@ const Profile = () => {
     }
   };
 
+  const handleProfilePicUpload = async (e) => {
+    const file = e.target.files[0];
+    const formData = new FormData();
+    formData.append('profilePic', file);
+
+    try {
+      const response = await axios.post('/api/uploadProfilePic', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+      setProfilePic(response.data.profilePicUrl);
+    } catch (error) {
+      console.error('Error uploading profile picture:', error);
+    }
+  };
+
+  const handleProfilePicRemove = async () => {
+    try {
+      await axios.post('/api/removeProfilePic');
+      setProfilePic(null);
+    } catch (error) {
+      console.error('Error removing profile picture:', error);
+    }
+  };
+
   return (
     <div className="container w-full">
       <motion.div
@@ -52,16 +81,35 @@ const Profile = () => {
         transition={{ duration: 0.5 }}
         className="bg-white rounded-md shadow-md p-6 mt-10 w-full h-full"
       >
-        <div className="mb-4 border-2 inline-block">
-          <button onClick={() => window.history.back()} className="text-xl text-red-700 border-black rounded-full p-3">
+        <div className="mb-4 border-black inline-block">
+          <button onClick={() => window.history.back()} className="text-xl text-red-700 border-transparent rounded-full p-3">
             <FaArrowLeft />
           </button>
         </div>
         <h1 className="text-2xl font-bold mb-4">Profile Details</h1>
         {user ? (
           <div className="flex flex-col w-full gap-4">
-            <div className="bg-gray-100 rounded-md shadow-md p-4">
-              <p><span className='text-lg font-semibold'>Name:</span> {user.name}</p>
+            <div className="flex flex-col items-center mb-4">
+              <div className="relative">
+                <img
+                  src={profilePic || blank_profile}
+                  alt="Profile"
+                  className="w-32 h-32 rounded-full mb-4 border-4 border-gray-200 shadow-md"
+                />
+                {profilePic && (
+                  <div className="absolute top-0 right-0">
+                    <button
+                      onClick={handleProfilePicRemove}
+                      className="text-red-500 hover:underline focus:outline-none"
+                    >
+                      Remove Image
+                    </button>
+                  </div>
+                )}
+              </div>
+              <div className="bg-gray-100 rounded-md shadow-md p-4 w-full">
+                <p><span className='text-lg font-semibold'>Name:</span> {user.name}</p>
+              </div>
             </div>
             <div className="bg-gray-100 rounded-md shadow-md p-4">
               <p><span className='text-lg font-semibold'>Username:</span> {user.username}</p>
@@ -70,10 +118,9 @@ const Profile = () => {
               <p><span className='text-lg font-semibold'>Verified:</span> {user.isVerified ? 'Already Verified' : 'Not Verified'}</p>
               {!user.isVerified && (
                 <button onClick={handleVerification} className="text-blue-500 hover:underline focus:outline-none">
-                  <Link  href={`/user/${user.username}/verifyAccount`}>
-                  Click here to verify
+                  <Link href={`/user/${user.username}/verifyAccount`}>
+                    Click here to verify
                   </Link>
-                 
                 </button>
               )}
             </div>
@@ -97,7 +144,7 @@ const Profile = () => {
               <p><span className='text-lg font-semibold'>Shop Address:</span> {user.shopAddress}</p>
             </div>
             <div className="bg-gray-100 rounded-md shadow-md p-4">
-              <p><span className='text-lg font-semibold'>Legal257 Id:</span> {user.id}</p>
+              <p><span className='text-lg font-semibold'>Legal Id:</span> {user.id}</p>
             </div>
           </div>
         ) : (
