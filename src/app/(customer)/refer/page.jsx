50f10531a97f17ea450ff62d4d39e431e02ef090
niks-nikhil-anand <niks.anand.developer@gmail.com>
2024-07-09 13:43:19 +0530

    Added some css

--- src/app/(customer)/refer/page.jsx
+++ src/app/(customer)/refer/page.jsx
@@ -154,25 +154,29 @@ const ReferForm = () => {
               required
             />
           </div>
-          <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="service">
-              Select Financial Service
-            </label>
-            <select
-              id="service"
-              name="service"
-              value={formData.service}
-              onChange={handleChange}
-              className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
-            required
-            >
-              <option value="" disabled>Select Financial Service</option>
-              <option value="Personal Loan">Personal Loan</option>
-              <option value="Business Loan">Business Loan</option>
-              <option value="Loan Against Property">Loan Against Property</option>
-              <option value="Home Loan">Home Loan</option>
-            </select>
-          </div>
+          <div className="mb-4 relative">
+  <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="service">
+    Select Financial Service
+  </label>
+  <select
+    id="service"
+    name="service"
+    value={formData.service}
+    onChange={handleChange}
+    className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-10 rounded shadow leading-tight focus:outline-none focus:shadow-outline "
+    required
+  >
+    <option value="" disabled>Select Financial Service</option>
+    <option value="Personal Loan">Personal Loan</option>
+    <option value="Business Loan">Business Loan</option>
+    <option value="Loan Against Property">Loan Against Property</option>
+    <option value="Home Loan">Home Loan</option>
+  </select>
+  <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+    <svg className="fill-current h-4 w-4 mt-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M7 10l5 5 5-5H7z"/></svg>
+  </div>
+</div>
+
           <div className="mb-4">
             <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="referMobileNumber">
               Whatsapp Number (Whom you want to refer)
