f615551a558371ebc47bf68bfdcb6b05042842a3
niks-nikhil-anand <niks.anand.developer@gmail.com>
2024-07-09 13:30:15 +0530

    Added some css

--- src/app/(customer)/refer/page.jsx
+++ src/app/(customer)/refer/page.jsx
@@ -164,7 +164,7 @@ const ReferForm = () => {
               value={formData.service}
               onChange={handleChange}
               className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
-              required
+            required
             >
               <option value="" disabled>Select Financial Service</option>
               <option value="Personal Loan">Personal Loan</option>
