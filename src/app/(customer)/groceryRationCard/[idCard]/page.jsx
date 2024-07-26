6eabd89b1f15722c2e7f30ee2d5efa2fdd5c7d4d
niks-nikhil-anand <niks.anand.developer@gmail.com>
2024-07-26 13:14:33 +0530

    Added Qr Code

--- src/app/(customer)/groceryRationCard/[idCard]/page.jsx
+++ src/app/(customer)/groceryRationCard/[idCard]/page.jsx
@@ -5,6 +5,8 @@ import { FaArrowLeft, FaPhoneAlt, FaGlobe } from 'react-icons/fa';
 import { toPng } from 'html-to-image';
 import Image from 'next/image';
 import stamp from '../../../../../public/vegetables/stamp.png'; 
+import BanStamp from '../../../../../public/vegetables/banStamp.png'; 
+import QRCode from 'qrcode.react'; 
 
 const formatDateTime = (dateString) => {
   const date = new Date(dateString);
@@ -19,13 +21,13 @@ const formatDateTime = (dateString) => {
 const GroceryRationCard = () => {
   const [rationCard, setRationCard] = useState(null);
   const [loading, setLoading] = useState(true);
-  
+  const [email, setEmail] = useState('');
 
-  
-useEffect(() => {
+  useEffect(() => {
     const fetchRationCard = async () => {
       try {
         const emailFromURL = window.location.pathname.split('/')[2];
+        setEmail(emailFromURL);
         const response = await fetch(`/api/groceryRationCard/${emailFromURL}`);
         if (response.ok) {
           const result = await response.json();
@@ -76,22 +78,30 @@ useEffect(() => {
               animate={{ opacity: 1, scale: 1 }}
               transition={{ duration: 0.5 }}
             >
-            <div className="bg-green-500 h-28 justify-center items-center relative rounded-t-lg flex flex-col py-5 shadow-2xl">
-              <h1 className="text-sm text-black font-extrabold mt-12 underline">Jono Jivan Grocery Ration Card</h1>
-              {rationCard.profilePhoto && (
-                <Image
-                  src={rationCard.profilePhoto}
-                  alt="Profile"
-                  height={100}
-                  width={100}
-                  className="border-4 border-white shadow-lg mt-3 rounded-full object-cover"
-                  style={{ objectFit: 'cover', width: '100px', height: '100px' }}
-                />
-              )}
-            </div>
+              <div className="bg-green-500 h-28 justify-center items-center relative rounded-t-lg flex flex-col py-5 shadow-2xl">
+                <h1 className="text-sm text-black font-extrabold mt-12 underline">Jono Jivan Grocery Ration Card</h1>
+                {rationCard.profilePhoto && (
+                  <Image
+                    src={rationCard.profilePhoto}
+                    alt="Profile"
+                    height={100}
+                    width={100}
+                    className="border-4 border-white shadow-lg mt-3 rounded-full object-cover"
+                    style={{ objectFit: 'cover', width: '100px', height: '100px' }}
+                  />
+                )}
+              </div>
 
-
-              <div className="absolute top-10 right-3  p-2">
+              <div className="absolute top-10 right-3 p-2">
+              {rationCard.status === 'Blocked' ? (
+                  <Image
+                    src={BanStamp}
+                    alt="Blocked Stamp"
+                    height={100}
+                    width={100}
+                    className="opacity-70"
+                  />
+                ) : (
                   <Image
                     src={stamp}
                     alt="Stamp"
@@ -99,88 +109,95 @@ useEffect(() => {
                     width={100}
                     className="opacity-70"
                   />
-                </div>
-              <div className="text-center mt-10 flex justify-start flex-col  mb-3">
+                )}
+              </div>
+              <div className="text-center mt-10 flex justify-start flex-col mb-3">
 
                 <p className="flex items-center font-light justify-end px-2">
-                <span className = "underline">Grocery Ration Card:- </span> <span className=''>#{rationCard.uniqueNumber}</span>
+                  <span className="underline">Grocery Ration Card:- </span> <span>#{rationCard.uniqueNumber}</span>
                 </p>
                 
                 <h2 className="text-xl text-blue-700 font-extrabold m-5 underline">{rationCard.name}</h2>
-                <div className=" text-sm flex flex-col flex-start px-2 ">
-                  <p className="text-gray-600"><span className="font-bold"> Father&apos;s Name:</span> {rationCard.fatherName}</p>
+                <div className="text-sm flex flex-col flex-start px-2">
+                  
+                  <p className="text-gray-600"><span className="font-bold">Father&apos;s Name:</span> {rationCard.fatherName}</p>
                   <p><span className="font-bold">WhatsApp No:</span> {rationCard.whatsAppNumber}</p>
                   <p><span className="font-bold">Mobile:</span> {rationCard.mobileNumber}</p>
                   <p><span className="font-bold">Email: </span>{rationCard.email}</p>
-                  <p> <span className="font-bold">Aadhaar No: </span>{rationCard.aadhaarNumber}</p>
-                  <p><span className = "font-bold">PAN No:</span> {rationCard.panNumber}</p>
-                  <p className="font-bold mt-2">Address:</p>
-                  <p>Add: {rationCard.address}, {rationCard.district},</p>
-                  <p>State: {rationCard.state}</p>
-                  <p>Pin Code: {rationCard.pinCode}</p>
+                  <p><span className="font-bold">Aadhaar No: </span>{rationCard.aadhaarNumber}</p>
+                  <p><span className="font-bold">PAN No:</span> {rationCard.panNumber}</p>
                   <p className="font-bold mt-2">Bank Details:</p>
                   <p>Account No: {rationCard.bankAccountNumber}</p>
                   <p>IFSC: {rationCard.ifscCode}</p>
                   <p>Bank Name: {rationCard.bankName}</p>
+                 
+                </div>
+
+                <div className='m-5'>
+                {email && (
+                  <div className="flex justify-center mt-4 items-center">
+                    <QRCode value={`www.legal257.in/groceryRationCard/${email}`} size={128} />
+                  </div>
+                )}
                 </div>
                 
-                <hr/>
+                <hr />
                 <div className='px-5 mt-5'>
-                <p className="flex items-center ">
-                <span className= "font-bold">Date of Issue: </span>   {formatDateTime(rationCard.dateOfIssue)}
-    </p>
-              </div>
+                  <p className="flex items-center ">
+                    <span className="font-bold">Date of Issue: </span> {formatDateTime(rationCard.dateOfIssue)}
+                  </p>
+                </div>
               </div>
             </motion.div>
 
             {/* Back Side of the Card */}
             <motion.div
-  className="w-full md:w-64 h-auto bg-green-700 rounded-lg shadow-lg overflow-hidden relative"
-  initial={{ opacity: 0, scale: 0.8 }}
-  animate={{ opacity: 1, scale: 1 }}
-  transition={{ duration: 0.5, delay: 0.2 }}
->
-  <div className="p-4 text-white text-xs md:text-sm space-y-2">
-    <h2 className="text-lg md:text-xl font-bold text-center mb-3">Terms and Conditions</h2>
-    <ul className="list-disc list-inside space-y-2">
-      <li className="font-semibold text-base">Ration Card Details:</li>
-      <ul className="list-inside ml-7 space-y-1 text-sm">
-        <li><span className = "font-medium">Ration Card Type:</span> Free</li>
-        <li><span className = "font-medium">Validity:</span>  11 months</li>
-        <li><span className = "font-medium">Insurance Coverage:</span> ₹1 lakh</li>
-        <li><span className = "font-medium">Eligibility:</span> Age 18 to 45</li>
-        <li><span className = "font-medium">Usage:</span> For grocery use only(not for personal use)</li>
-        <li> </li>
-      </ul>
-      <li className="font-semibold text-base">Shop Details:</li>
-      <ul className="list-inside ml-7 space-y-1 text-sm">
-      <li><span className = "font-medium">Opening Time:</span>  7:00 AM</li>
-      <li><span className = "font-medium">Closing Time:</span>  9:00 PM</li>
-      </ul>
-      <li className="font-semibold text-base">Services Provided:</li>
-      <ul className="list-inside ml-7 space-y-1 text-sm">
-      <li><span className = "font-medium">Grocery Home Delivery:</span>  Available</li>
-      <li><span className = "font-medium">Grocery Home Delivery:</span>  Available</li>
-
-       <div>
-       <li>JonoJivan Grocery Ration Card</li>
-        <li>Digital Banking Service</li>
-        <li>Financial Services</li>
-        <li>All Types of Finance Loan Services</li>
-       </div>
-        
-      </ul>
-    </ul>
-    <hr/>
-    <p className="flex items-center mt-3 my-5">
-      <FaPhoneAlt className="mr-2 text-xl" />+91 8761873802
-    </p>
-    <p className="flex items-center">
+              className="w-full md:w-64 h-auto bg-green-700 rounded-lg shadow-lg overflow-hidden relative"
+              initial={{ opacity: 0, scale: 0.8 }}
+              animate={{ opacity: 1, scale: 1 }}
+              transition={{ duration: 0.5, delay: 0.2 }}
+            >
+              <div className="p-4 text-white text-xs md:text-sm space-y-2">
+                <h2 className="text-lg md:text-xl font-bold text-center mb-3">Terms and Conditions</h2>
+                <ul className="list-disc list-inside space-y-2">
+                  <li className="font-semibold text-base">Ration Card Details:</li>
+                  <ul className="list-inside ml-7 space-y-1 text-sm">
+                    <li><span className="font-medium">Ration Card Type:</span> Free</li>
+                    <li><span className="font-medium">Validity:</span> 11 months</li>
+                    <li><span className="font-medium">Insurance Coverage:</span> ₹1 lakh</li>
+                    <li><span className="font-medium">Eligibility:</span> Age 18 to 45</li>
+                    <li><span className="font-medium">Usage:</span> For grocery use only (not for personal use)</li>
+                  </ul>
+                  <li className="font-semibold text-base">Shop Details:</li>
+                  <ul className="list-inside ml-7 space-y-1 text-sm">
+                    <li><span className="font-medium">Opening Time:</span> 7:00 AM</li>
+                    <li><span className="font-medium">Closing Time:</span> 9:00 PM</li>
+                  </ul>
+                  <li className="font-semibold text-base">Services Provided:</li>
+                  <ul className="list-inside ml-7 space-y-1 text-sm">
+                    <li><span className="font-medium">Grocery Home Delivery:</span> Available</li>
+                    <li><span className="font-medium">Digital Banking Service:</span> Available</li>
+                    <li><span className="font-medium">Financial Services:</span> Available</li>
+                    <li><span className="font-medium">All Types of Finance Loan Services:</span> Available</li>
+                  </ul>
+                </ul>
+                <hr />
+                <p className="font-bold mt-2">Address:</p>
+                  <p>Add: {rationCard.address}, {rationCard.district},</p>
+                  <p>State: {rationCard.state}</p>
+                  <p>Pin Code: {rationCard.pinCode}</p>
+                 
+                  <hr/>
+                <p className="flex items-center mt-3 my-5">
+                  <FaPhoneAlt className="mr-2 text-xl" />+91 8761873802
+                </p>
+                <p className="flex items-center">
                   <FaGlobe className="mr-2 text-2xl" /> <a href="http://legal257.in" className="underline">www.legal257.in</a>
                 </p>
-  </div>
-</motion.div>
-
+                {/* QR Code */}
+               
+              </div>
+            </motion.div>
           </div>
         </div>
       ) : (
