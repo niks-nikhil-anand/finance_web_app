982de1468ca12daa7ad9af50b3d9b793e93acd7f
niks-nikhil-anand <niks.anand.developer@gmail.com>
2024-08-07 14:56:40 +0530

    Added loader in the user panel

--- src/app/(user)/user/[username]/page.jsx
+++ src/app/(user)/user/[username]/page.jsx
@@ -1,12 +1,28 @@
+import React, { useState, useEffect } from 'react'
 import ColorfulCard from '@/component/User/Card'
-import React from 'react'
 
-const page = () => {
+const Page = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2000); // Simulating a 2-second loading time
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <div className='md:h-[75vh]'>
-      <ColorfulCard/>
+      {loading ? (
+        <div className="flex justify-center items-center h-full">
+          <div className="loader"></div>
+        </div>
+      ) : (
+        <ColorfulCard />
+      )}
     </div>
   )
 }
 
-export default page
+export default Page
