d91a529eca1f9218fe1a35f5a6fc008908dbc7d3
niks-nikhil-anand <niks.anand.developer@gmail.com>
2024-12-16 11:59:48 +0530

    Added hover in the navbar

--- src/component/Homepage/Navbar.jsx
+++ src/component/Homepage/Navbar.jsx
@@ -27,90 +27,163 @@ const Navbar = () => {
   return (
     <nav className="bg-gray-600 text-white">
   <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-    <div className="flex justify-between items-center h-16">
-      <div className="flex items-center">
-        <div className="flex">
-          <Link href="/">
-            <Image src={logo} alt="TailGrids" height={100} width={100} className="w-[12rem] md:w-[7rem]" />
-          </Link>
-        </div>
-        <div className="hidden md:flex ml-10 items-baseline space-x-4">
-          <Link href="/" className="px-3 py-2 rounded-md text-sm font-medium">Home</Link>
-          <div className="relative inline-block text-left">
-            <button
-              onClick={toggleSubMenu(setLoanCalcOpen, loanCalcOpen)}
-              type="button"
-              className="px-3 py-2 rounded-md text-sm font-medium"
-            >
-              Loan Calculator &#x25BE;
-            </button>
-            <motion.div
-              initial={{ opacity: 0, y: -10 }}
-              animate={{ opacity: loanCalcOpen ? 1 : 0, y: loanCalcOpen ? 0 : -10 }}
-              transition={{ duration: 0.2 }}
-              className={`absolute z-10 mt-2 w-48 bg-white rounded-md shadow-lg ${loanCalcOpen ? "block" : "hidden"}`}
-            >
-              <Link href="/calculator" onClick={handleLinkClick} className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Monthly EMI Calculator</Link>
-              <Link href="/applyloan" onClick={handleLinkClick} className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Eligibility Calculator</Link>
-            </motion.div>
-          </div>
-          <div className="relative inline-block text-left">
-            <button
-              onClick={toggleSubMenu(setLoansOpen, loansOpen)}
-              type="button"
-              className="px-3 py-2 rounded-md text-sm font-medium"
-            >
-              Loans &#x25BE;
-            </button>
-            <motion.div
-              initial={{ opacity: 0, y: -10 }}
-              animate={{ opacity: loansOpen ? 1 : 0, y: loansOpen ? 0 : -10 }}
-              transition={{ duration: 0.2 }}
-              className={`absolute z-10 mt-2 w-48 bg-white rounded-md shadow-lg ${loansOpen ? "block" : "hidden"}`}
-            >
-              <Link href="/personalloan" onClick={handleLinkClick} className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Personal Loan</Link>
-              <Link href="/businessloan" onClick={handleLinkClick} className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Business Loan</Link>
-              <Link href="/loanproperty" onClick={handleLinkClick} className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Loan Against Property</Link>
-              <Link href="/homeloan" onClick={handleLinkClick} className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Home Loan</Link>
-            </motion.div>
-          </div>
-          <Link href="/career" className="px-3 py-2 rounded-md text-sm font-medium">Career</Link>
-          <Link href="/about" className="px-3 py-2 rounded-md text-sm font-medium">About Us</Link>
-          <Link href="/photogallery" className="px-3 py-2 rounded-md text-sm font-medium">Media Gallery</Link>
-          <Link href="/contact" className="px-3 py-2 rounded-md text-sm font-medium">Contact Us</Link>
-          <Link href="/partnersignup" className="px-3 py-2 rounded-md text-sm font-medium">Become Our Partner</Link>
-        </div>
-      </div>
-      {/* Desktop Sign In button */}
-      <div className="hidden md:flex ml-4 items-center md:ml-6">
-        <Link href="/partnersignin">
-          <motion.button
-            className="px-4 py-1 font-medium text-black transition-all bg-yellow-400  rounded-lg shadow-lg hover:shadow-xl hover:translate-x-[3px] hover:translate-y-[3px]"
-            whileHover={{ scale: 1.05 }}
-            whileTap={{ scale: 0.95 }}
-          >
-            Sign In
-          </motion.button>
+  <div className="flex justify-between items-center h-16">
+    <div className="flex items-center">
+      <div className="flex">
+        <Link href="/">
+          <Image
+            src={logo}
+            alt="TailGrids"
+            height={100}
+            width={100}
+            className="w-[12rem] md:w-[7rem] hover:opacity-80 transition duration-300"
+          />
         </Link>
       </div>
-      <div className="-mr-2 flex md:hidden ">
-        <button
-          onClick={() => setIsOpen(!isOpen)}
-          type="button"
-          className="bg-yellow-500 inline-flex items-center justify-center p-2 rounded-md text-white hover:text-gray-300 hover:bg-yellow-500"
-          aria-controls="mobile-menu"
-          aria-expanded="false"
+      <div className="hidden md:flex ml-10 items-baseline space-x-4">
+        <Link
+          href="/"
+          className="px-3 py-2 rounded-md text-sm font-medium hover:text-yellow-500 transition duration-300"
         >
-          <span className="sr-only">Open main menu</span>
-          {isOpen ? (
-            <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
-          ) : (
-            <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
-          )}
-        </button>
+          Home
+        </Link>
+        <div className="relative inline-block text-left">
+          <button
+            onClick={toggleSubMenu(setLoanCalcOpen, loanCalcOpen)}
+            type="button"
+            className="px-3 py-2 rounded-md text-sm font-medium hover:text-yellow-500 transition duration-300"
+          >
+            Loan Calculator &#x25BE;
+          </button>
+          <motion.div
+            initial={{ opacity: 0, y: -10 }}
+            animate={{ opacity: loanCalcOpen ? 1 : 0, y: loanCalcOpen ? 0 : -10 }}
+            transition={{ duration: 0.2 }}
+            className={`absolute z-10 mt-2 w-48 bg-white rounded-md shadow-lg ${loanCalcOpen ? "block" : "hidden"}`}
+          >
+            <Link
+              href="/calculator"
+              onClick={handleLinkClick}
+              className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 transition duration-200"
+            >
+              Monthly EMI Calculator
+            </Link>
+            <Link
+              href="/applyloan"
+              onClick={handleLinkClick}
+              className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 transition duration-200"
+            >
+              Eligibility Calculator
+            </Link>
+          </motion.div>
+        </div>
+        <div className="relative inline-block text-left">
+          <button
+            onClick={toggleSubMenu(setLoansOpen, loansOpen)}
+            type="button"
+            className="px-3 py-2 rounded-md text-sm font-medium hover:text-yellow-500 transition duration-300"
+          >
+            Loans &#x25BE;
+          </button>
+          <motion.div
+            initial={{ opacity: 0, y: -10 }}
+            animate={{ opacity: loansOpen ? 1 : 0, y: loansOpen ? 0 : -10 }}
+            transition={{ duration: 0.2 }}
+            className={`absolute z-10 mt-2 w-48 bg-white rounded-md shadow-lg ${loansOpen ? "block" : "hidden"}`}
+          >
+            <Link
+              href="/personalloan"
+              onClick={handleLinkClick}
+              className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 transition duration-200"
+            >
+              Personal Loan
+            </Link>
+            <Link
+              href="/businessloan"
+              onClick={handleLinkClick}
+              className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 transition duration-200"
+            >
+              Business Loan
+            </Link>
+            <Link
+              href="/loanproperty"
+              onClick={handleLinkClick}
+              className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 transition duration-200"
+            >
+              Loan Against Property
+            </Link>
+            <Link
+              href="/homeloan"
+              onClick={handleLinkClick}
+              className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 transition duration-200"
+            >
+              Home Loan
+            </Link>
+          </motion.div>
+        </div>
+        <Link
+          href="/career"
+          className="px-3 py-2 rounded-md text-sm font-medium hover:text-yellow-500 transition duration-300"
+        >
+          Career
+        </Link>
+        <Link
+          href="/about"
+          className="px-3 py-2 rounded-md text-sm font-medium hover:text-yellow-500 transition duration-300"
+        >
+          About Us
+        </Link>
+        <Link
+          href="/photogallery"
+          className="px-3 py-2 rounded-md text-sm font-medium hover:text-yellow-500 transition duration-300"
+        >
+          Media Gallery
+        </Link>
+        <Link
+          href="/contact"
+          className="px-3 py-2 rounded-md text-sm font-medium hover:text-yellow-500 transition duration-300"
+        >
+          Contact Us
+        </Link>
+        <Link
+          href="/partnersignup"
+          className="px-3 py-2 rounded-md text-sm font-medium hover:text-yellow-500 transition duration-300"
+        >
+          Become Our Partner
+        </Link>
       </div>
     </div>
+    {/* Desktop Sign In button */}
+    <div className="hidden md:flex ml-4 items-center md:ml-6">
+      <Link href="/partnersignin">
+        <motion.button
+          className="px-4 py-1 font-medium text-black transition-all bg-yellow-400  rounded-lg shadow-lg hover:shadow-xl hover:translate-x-[3px] hover:translate-y-[3px] hover:bg-yellow-500 transition duration-300"
+          whileHover={{ scale: 1.05 }}
+          whileTap={{ scale: 0.95 }}
+        >
+          Sign In
+        </motion.button>
+      </Link>
+    </div>
+    <div className="-mr-2 flex md:hidden">
+      <button
+        onClick={() => setIsOpen(!isOpen)}
+        type="button"
+        className="bg-yellow-500 inline-flex items-center justify-center p-2 rounded-md text-white hover:text-gray-300 hover:bg-yellow-500 transition duration-300"
+        aria-controls="mobile-menu"
+        aria-expanded="false"
+      >
+        <span className="sr-only">Open main menu</span>
+        {isOpen ? (
+          <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+        ) : (
+          <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+        )}
+      </button>
+    </div>
   </div>
+</div>
+
 
       {/* Mobile menu */}
       <motion.div
